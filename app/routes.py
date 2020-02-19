da458a63329c2ab4e901cffd5aafe1efa8c20324
chrisjamesr <chrisjames.r@gmail.com>
2020-02-18 19:28:13 -0500

    import render_templates and return function call to render index template from index route

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from app import app
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    user={'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
