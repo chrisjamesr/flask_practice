6b8d583cfd2c65cbd7881426d6c96e77967ee512
chrisjamesr <chrisjames.r@gmail.com>
2020-02-19 21:28:05 -0500

    fix typo

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from flask-wtf import FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
