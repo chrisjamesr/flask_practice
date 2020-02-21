e25128599945ebb54e09a2a4bdcdaf34d8ce72ca
chrisjamesr <chrisjames.r@gmail.com>
2020-02-21 13:00:10 -0500

    Import login from app. Register user loader with login user_loader decorator.

--- app/models.py
+++ app/models.py
@@ -2,7 +2,12 @@ from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from app import login
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+   
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
