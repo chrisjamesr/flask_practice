166f4ed96038c3c06fc9cd2d942a10d27b7aa631
chrisjamesr <chrisjames.r@gmail.com>
2020-02-21 13:14:17 -0500

    add logout route and function

--- app/routes.py
+++ app/routes.py
@@ -24,3 +24,8 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
