eb0725600a5a3355266dd9173057895cc08079e9
chrisjamesr <chrisjames.r@gmail.com>
2020-03-05 13:23:21 -0500

    add methods to root & index routes. Add all posts to view function

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post
 from werkzeug.urls import url_parse
@@ -12,12 +12,19 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/index', methods = ['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = Post.query.all() 
-    return render_template('index.html', title='Home Page', posts=posts)
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -63,10 +70,7 @@ def users():
 @app.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test Post #1'},
-        {'author': user, 'body': 'Test Post #2'}
-    ]
+    posts = Post.query.filter_by(user_id=user.id)
     return render_template('user.html', user=user, posts=posts)
    
 
