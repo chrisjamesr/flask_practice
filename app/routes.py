99b76ac58b0a6cf05f9f7aff124e242df6beca27
chrisjamesr <chrisjames.r@gmail.com>
2020-03-05 21:46:46 -0500

    add pagination to index and explore routes

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,7 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
+
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/index', methods = ['GET', 'POST'])
 @login_required
@@ -24,8 +25,20 @@ def index():
         flash('Your post is now live!')
         return redirect(url_for('index'))
     # posts = Post.query.all() 
-    posts = current_user.followed_posts().all()
-    return render_template('index.html', title='Home Page', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False) 
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', 
+        title='Home Page', 
+        form=form, 
+        posts=posts.items,
+        next_url = next_url,
+        prev_url = prev_url
+    )
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -66,8 +79,16 @@ def register():
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("index.html", title='Explore', posts=posts.items,
+                          next_url=next_url, prev_url=prev_url)
+
 
 @app.route('/users')
 def users():
