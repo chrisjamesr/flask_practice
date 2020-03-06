23eca6971d472e6c0f7be538bceb8503b45da8e5
chrisjamesr <chrisjames.r@gmail.com>
2020-03-05 20:51:33 -0500

    send followed posts to index template

--- app/routes.py
+++ app/routes.py
@@ -23,7 +23,8 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = Post.query.all() 
+    # posts = Post.query.all() 
+    posts = current_user.followed_posts().all()
     return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods=['GET','POST'])
