9d485598b3f3abaed0e4e640c9abb4b76b916dcf
chrisjamesr <chrisjames.r@gmail.com>
2020-03-12 13:06:57 -0400

    import threading from Thread

--- app/email.py
+++ app/email.py
@@ -1,6 +1,7 @@
 from flask_mail import Message
 from flask import render_template
 from app import mail, app
+from threading import Thread
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
