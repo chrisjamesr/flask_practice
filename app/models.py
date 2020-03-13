24af8e15912c1ac70f26a7ed95e29089f936c72b
chrisjamesr <chrisjames.r@gmail.com>
2020-03-13 13:20:28 -0400

    import relevant modules and add functions for password reset tokens

--- app/models.py
+++ app/models.py
@@ -2,8 +2,10 @@ from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import login
+from app import app,login
 from hashlib import md5
+from time import time
+import jwt
 
 @login.user_loader
 def load_user(id):
